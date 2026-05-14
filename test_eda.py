4c8dc5ada9a50774b7c132afbcff75fa3d85b642
Den4ikg1 <dan201683@yahoo.com>
2026-05-13 20:36:12 -0700

    created test_utils

--- test_eda.py
+++ test_eda.py
@@ -1,8 +1,22 @@
 """
 Tests eda.py for correctness.
-daniil
+Daniil Kim
 """
-
+import eda
+import data_utils
+# either import the functions and test them, or whole thign and use the fuctnions inside them
+#See the data sets
+'''
+Write assert-based tests for:
+    * clean_housing_units() returns correct shape
+    * clean_population() aggregates correctly
+    * merge_datasets() produces expected columns
+    * derived columns (units per capita) compute correctly
+- Run: python test_eda.py — should pass with no errors
+'''
+housing = load_housing_units('data/housing_units.csv')
+population = load_population('data/population.csv')
+hpi = load_hpi('data/hpi_master.csv')
 
 def main() -> None:
     pass
